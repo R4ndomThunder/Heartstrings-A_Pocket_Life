9a6399cd6ce6cb8e2fe301a6f188c0b7da7a2930
R4ndomThunder <samuelepadalino@gmail.com>
2024-11-22 21:26:06 +0100

    RTDK + Transparent Window

---
+++ Assets/_Project/Scripts/TransparentWindow.cs
@@ -0,0 +1,73 @@
+using RTDK.Utility;
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TransparentWindow : MonoBehaviour
+{
+    private struct MARGINS
+    {
+        public int cxLeftWidth;
+        public int cxRightWidth;
+        public int cyTopHeight;
+        public int cyBottomHeight;
+    }
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+    [DllImport("Dwmapi.dll")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
+
+    [DllImport("user32.dll")]
+    private static extern int SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+
+    const int GWL_EXSTYLE = -20;
+
+    const uint WS_EX_LAYERED = 0x00080000;
+    const uint WS_EX_TRANSPARENT = 0x00000020;
+
+    static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+    const uint LWA_COLORKEY = 0x00000001;
+
+    private IntPtr hWnd;
+
+    private void Start()
+    {
+#if !UNITY_EDITOR
+        hWnd = GetActiveWindow();
+
+        MARGINS margins = new MARGINS { cxLeftWidth = -1 };
+
+        DwmExtendFrameIntoClientArea(hWnd, ref margins);
+
+        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
+
+        SetLayeredWindowAttributes(hWnd, 0,0,LWA_COLORKEY);
+
+        SetWindowPos(hWnd, HWND_TOPMOST, 0,0,0,0,0);
+#endif
+        Application.runInBackground = true;
+    }
+
+    private void Update()
+    {
+        SetClickthrough(Physics2D.OverlapPoint(Utils.Camera.ScreenToWorldPoint(Input.mousePosition)) == null);
+    }
+
+    void SetClickthrough(bool clickThrough)
+    {
+        if (clickThrough)
+        {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+        else
+        {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
+        }
+    }
+}
